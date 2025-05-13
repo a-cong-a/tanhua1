56477f901e7eb33dd8a115dafb25535fb3237db9
stepanie1996 <130530497+stepanie1996@users.noreply.github.com>
2025-05-13 10:13:49 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -60,7 +60,7 @@ const BLOG = {
     process.env.NEXT_PUBLIC_LAYOUT_SIDEBAR_REVERSE || false,
 
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
-  GREETING_WORDS:true
+  GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
     '欢迎来到91狼友之家',
 
