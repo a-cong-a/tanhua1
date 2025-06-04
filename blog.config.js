d0b6d461cb0365d6b719450d5373f2f462eb5b45
leyeirinuca <leyeirinuca@gmail.com>
2025-06-04 17:14:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,10 +14,10 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '91狼友之家', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '其实你想看的，网上都有', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '项目小组', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个项目', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://91home.top', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '91, 91home, 91狼友之家, 暗网, 呦呦, 91porn, 成人网站, 色情网站, 宅男福利, 福利姬, 强奸, 迷奸, 乱伦, 草榴, 2048, 幼幼, 缅北, 人兽', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '副业搞钱', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
